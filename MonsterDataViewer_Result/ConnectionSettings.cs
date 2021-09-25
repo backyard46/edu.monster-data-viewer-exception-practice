755b57959ef7a96024b39d41dc570546304d71be
backyarD <backyard46@gmail.com>
2021-09-25 17:18:31 +0900

    Update ConnectionSettings.cs

--- MonsterDataViewer_Result/ConnectionSettings.cs
+++ MonsterDataViewer_Result/ConnectionSettings.cs
@@ -121,21 +121,9 @@ namespace MonsterDataViewer
             // 選択肢等を初期設定
 
             // for 2020
-            comboServer.Items.AddRange(new string[] { "localhost", "localhost\\SQLEXPRESS", "SIGMA-WSV009" });
+            comboServer.Items.AddRange(new string[] { "localhost", "localhost\\SQLEXPRESS", "WSV009" });
             comboDatabase.Items.AddRange(new string[] { "kadai010" });
-            comboID.Items.AddRange(new string[] { "hattori", "hirao", "hosaka", "murayama", "toyooka" });
-
-            // for 2019
-            //comboServer.Items.Add("SIGMA-WSV009");
-            //comboServer.Items.Add("SN17002");
-            //comboDatabase.Items.AddRange(new string[] { "miura", "nishiyama", "tokiwa" });
-            //comboID.Items.AddRange(new string[] { "miura_admin", "nishiyama_admin", "tokiwa_admin" });
-
-            // for 2018
-            //comboServer.Items.Add("SIGMA-WSV009");
-            //comboServer.Items.Add("SN17002");
-            //comboDatabase.Items.AddRange(new string[] { "iguchi", "kasai", "kikuchi", "watanuki" });
-            //comboID.Items.AddRange(new string[] { "iguchi_admin", "kasai_admin", "kikuchi_admin", "watanuki_admin" });
+            comboID.Items.AddRange(new string[] { "userA", "userB", "userC", "userD", "userE" });
 
             // コンストラクターで渡された情報を初期設定する
             comboServer.Text = _serverName;
